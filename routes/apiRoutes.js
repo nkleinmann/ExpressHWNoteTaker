7d0ff309a441fad71dd0f401118c701dc2e7e680
nkleinmann <nekleinmann@gmail.com>
2020-09-05 12:22:11 -0400

    getNotes and changeNotes functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,8 +5,29 @@ const { v4: uuidv4 } = require('uuid');
 
 let userNotes = [];
 
-// fs.readFile("./db/db.json", (error, data) => {
-//     if (error) throw error;
-//     below line?
-//     // userNotes = (JSON.parse(data));
-// } 
+fs.readFile("./db/db.json", (error, data) => {
+    if (error) throw error;
+    userNotes = (JSON.parse(data));
+}); 
+
+function changeNote() {
+        fs.writeFile("./db/db.json", JSON.stringify(userNotes), (error) => {
+            if (error) throw error;
+        });
+    }
+
+function getNotes(notes) {
+    notes.get("/api/notes", (request, response) => {
+        response.json(userNotes);
+    });
+
+    notes.post("/api/notes", (request, response) => {
+        const anotherNote = request.body;
+        anotherNote.id = (uuidv4());
+        userNotes.push(anotherNote);
+        changeNote();
+        response.send(userNotes);
+    });
+}
+
+module.exports = getNotes;
